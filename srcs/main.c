fa2ff1faed2d46014082c5257b45c1ae110a0ab1
Noa Moulin <noa.moulin42@gmail.com>
2023-02-10 23:43:11 +0100

    fini ?

--- srcs/main.c
+++ srcs/main.c
@@ -16,14 +16,17 @@ int	main(int ac, char **av)
 {
 	t_push_swap	*push_swap_;
 
-	if (!entry_is_valid(--ac, ++av))
-		invalid_entry_error();
-	push_swap_ = push_swap(ac);
-	if (!push_swap_)
-		allocation_error();
-	init_push_swap(push_swap_, av, ac);
-	radix_sort(push_swap_);
-	print_str(push_swap_->instructions);
-	delete_push_swap(push_swap_);
+	if (ac > 1)
+	{
+		if (!entry_is_valid(--ac, ++av))
+			invalid_entry_error();
+		push_swap_ = push_swap(ac);
+		if (!push_swap_)
+			allocation_error();
+		init_push_swap(push_swap_, av, ac);
+		radix_sort(push_swap_);
+		print_str(push_swap_->instructions);
+		delete_push_swap(push_swap_);
+	}
 	return (0);
 }
--- srcs/radix.c
+++ srcs/radix.c
@@ -12,8 +12,37 @@
 
 #include "../push_swap.h"
 
+static uint8_t	max_bits(int32_t n)
+{
+	uint8_t	nb;
+
+	nb = 0;
+	while (n >> nb)
+		nb++;
+	return (nb);
+}
+
 void	radix_sort(t_push_swap *push_swap)
 {
-	(void)push_swap;
-	return ;
+	uint32_t	stack_index;
+	uint8_t		max_bit_number;
+	uint8_t		count;
+
+	max_bit_number = max_bits(push_swap->sorted_a[push_swap->stacks_len - 1]);
+	count = 0;
+	while (count < max_bit_number)
+	{
+		stack_index = 0;
+		while (stack_index < push_swap->stacks_len)
+		{
+			if (push_swap->stack_a[0] >> count & 1)
+				ra(push_swap);
+			else
+				pb(push_swap);
+			stack_index++;
+		}
+		while (push_swap->b_weight)
+			pa(push_swap);
+		count++;
+	}
 }
