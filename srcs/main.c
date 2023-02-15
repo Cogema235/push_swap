ddc7bc43bfe06f2911e7eec2cf0b20e693e57e90
Noa Moulin <noa.moulin42@gmail.com>
2023-02-15 01:41:34 +0100

    frfrfr

--- srcs/main.c
+++ srcs/main.c
@@ -12,15 +12,6 @@
 
 #include "../push_swap.h"
 
-void	display_stacks(t_push_swap *push_swap)
-{
-	for (unsigned int i = 0; i < push_swap->stacks_len; i++)
-	{
-		printf("%d ", push_swap->stack_a[i]);
-	}
-	printf("\n");
-}
-
 int	main(int ac, char **av)
 {
 	t_push_swap	*push_swap_;
@@ -33,7 +24,6 @@ int	main(int ac, char **av)
 		if (!push_swap_)
 			allocation_error();
 		init_push_swap(push_swap_, av, ac);
-		display_stacks(push_swap_);
 		if (!is_sorted(push_swap_->stack_a, push_swap_->stacks_len))
 		{
 			if (push_swap_->stacks_len == 2)
@@ -46,7 +36,6 @@ int	main(int ac, char **av)
 				radix_sort(push_swap_);
 			print_str(push_swap_->instructions);
 		}
-		display_stacks(push_swap_);
 		delete_push_swap(push_swap_);
 	}
 	return (0);
